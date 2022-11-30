e586ef3039711da46bd9aadc31143933c0f806eb
grade33 <baledg3@gmail.com>
2022-11-30 23:50:37 +0300

    first-commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/cryptonomicon/',
+  server: {
+    port: 8080,
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
